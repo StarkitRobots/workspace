71a9bb912462f778bf2f443f25939937fe6ebe68
Ilya Ryakin <ris2012.r@yandex.ru>
2019-04-10 15:03:51 +0300

    Update autoFormat.sh

--- autoFormat.sh
+++ autoFormat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-files=$( find src/rhoban -name "*.cpp" -or -name "*.h" -or -name "*.hpp" -or -name "*.cc" -or -name "*.hh" )
+files=$( find src/starkit -name "*.cpp" -or -name "*.h" -or -name "*.hpp" -or -name "*.cc" -or -name "*.hh" )
 
 clang-format -i -style=file --verbose ${files}
